b13045ec7aa84d473f81b4d1906ccdfeb97706b5
michland <michland@1e788866-ab73-4bbf-b906-69731a7f51b1>
2007-02-25 23:25:15 +0000

    Add licensing information

--- bench/vhdl/colorconv_tb.vhd
+++ bench/vhdl/colorconv_tb.vhd
@@ -1,4 +1,54 @@
------------------------------------------------------------------------------------
+-- ***** BEGIN LICENSE BLOCK *****
+----------------------------------------------------------------------
+----                                                              ----
+----  True matrix 3x3 color convertion IP Core                    ----
+----                                                              ----
+---- This file is part of the matrix 3x3 multiplier project       ----
+---- http://www.opencores.org/projects.cgi/web/matrix3x3/         ----
+----                                                              ----
+---- Description                                                  ----
+---- True matrix 3x3 color converter							  ----
+---- 		                                                      ----
+---- To Do:                                                       ----
+---- -                                                            ----
+----                                                              ----
+---- Author(s):                                                   ----
+---- - Michael Tsvetkov, michland@opencores.org                   ----
+---- - Vyacheslav Gulyaev, vv_gulyaev@opencores.org               ----	
+----                                                              ----
+----------------------------------------------------------------------
+----                                                              ----
+---- Copyright (C) 2006 Authors and OPENCORES.ORG                 ----
+----                                                              ----
+---- This source file may be used and distributed without         ----
+---- restriction provided that this copyright statement is not    ----
+---- removed from the file and that any derivative work contains  ----
+---- the original copyright notice and the associated disclaimer. ----
+----                                                              ----
+---- This source file is free software; you can redistribute it   ----
+---- and/or modify it under the terms of the GNU Lesser General   ----
+---- Public License as published by the Free Software Foundation; ----
+---- either version 2.1 of the License, or (at your option) any   ----
+---- later version.                                               ----
+----                                                              ----
+---- This source is distributed in the hope that it will be       ----
+---- useful, but WITHOUT ANY WARRANTY; without even the implied   ----
+---- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      ----
+---- PURPOSE. See the GNU Lesser General Public License for more  ----
+---- details.                                                     ----
+----                                                              ----
+---- You should have received a copy of the GNU Lesser General    ----
+---- Public License along with this source; if not, download it   ----
+---- from http://www.gnu.org/licenses/lgpl.txt or  write to the   ----
+---- Free Software Foundation, Inc., 51 Franklin Street,          ----
+---- Fifth Floor, Boston, MA  02110-1301  USA                     ----
+----                                                              ----
+----------------------------------------------------------------------
+-- * ***** END LICENSE BLOCK ***** */
+
+
+
+-----------------------------------------------------------------------
 --
 -- There is testbench for different color conversions by the mult3x3 component.
 -- 
@@ -11,7 +61,7 @@
 --
 -- Simulator software - ModelSim 6.1.
 --
------------------------------------------------------------------------------------
+-----------------------------------------------------------------------
 
 LIBRARY ieee;
 LIBRARY std_developerskit;
